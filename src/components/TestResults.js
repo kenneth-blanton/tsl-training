94bbec66ef9817ca87fe857366547cbb34fcf5f7
Kenneth-Blanton <knsylvest@gmail.com>
2023-04-02 16:29:22 -0500

    Solved Shuffle

--- src/components/TestResults.js
+++ src/components/TestResults.js
@@ -1,6 +1,6 @@
 const TestResults = (props) => {
   const { score } = props;
-  return <div className="TestResults">{score}</div>;
+  return <div className="TestResults">Score is: {score}</div>;
 };
 
 export default TestResults;
